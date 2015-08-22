19c95feb5362b6f56277ea12f672694624ca948f
Chris Casola <chriscasola@gmail.com>
2015-08-21 21:32:52 -0400

    chore(build): adding a build

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+var gulp = require('gulp');
+var del = require('del');
+var mergeStream = require('merge-stream');
+var plugins = require('gulp-load-plugins')({
+  rename: {
+    'gulp-babel-external-helpers': 'babelHelpers',
+  },
+});
+
+var config = {
+  dest: 'dist/',
+  src: 'src/**/*.js',
+  test: 'test/**/*.spec.js',
+};
+
+gulp.task('clean', function(done) {
+  del([config.dest], done);
+});
+
+function sourceFileStream() {
+  return gulp.src([config.src], {base: './'})
+    .pipe(plugins.plumber())
+    .pipe(plugins.eslint())
+    .pipe(plugins.eslint.format())
+    .pipe(plugins.eslint.failOnError())
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.babel({externalHelpers: true}))
+    .pipe(plugins.wrapJs('(function() { %= body % })()'))
+    .pipe(plugins.babelHelpers('babelHelpers.js', 'var'))
+    .pipe(plugins.concat('angular-smarter-models.js'))
+    .pipe(plugins.wrapJs('(function() { %= body % })()'))
+    .pipe(plugins.sourcemaps.write('./'))
+    .pipe(plugins.plumber.stop());
+}
+
+gulp.task( 'build', ['clean'], function() {
+  return sourceFileStream()
+    .pipe(plugins.plumber())
+    .pipe(gulp.dest(config.dest))
+    .pipe(plugins.ignore.exclude(/.js.map/))
+    .pipe(plugins.rename({ extname: '.min.js' }))
+    .pipe(plugins.uglify())
+    .pipe(plugins.sourcemaps.write('./'))
+    .pipe(gulp.dest(config.dest))
+    .pipe(plugins.plumber.stop());;
+});
+
+function testFileStream() {
+  return gulp.src(['test/**/*.spec.js'], {base: './'})
+    .pipe(plugins.plumber())
+    .pipe(plugins.eslint({
+      globals: {
+        'describe': true,
+        'it': true,
+        'expect': true,
+      },
+    }))
+    .pipe(plugins.eslint.format())
+    .pipe(plugins.eslint.failOnError())
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.babel({externalHelpers: true}))
+    .pipe(plugins.wrapJs('(function() { %= body % })()'))
+    .pipe(plugins.babelHelpers('babelHelpers.js', 'var'))
+    .pipe(plugins.concat('angular-smarter-models.spec.js'))
+    .pipe(plugins.sourcemaps.write('./'))
+    .pipe(plugins.plumber.stop());
+}
+
+function dependencyStream() {
+  return gulp.src('./bower.json')
+    .pipe(plugins.mainBowerFiles());
+}
+
+gulp.task('test', ['build'], function() {
+  var sourceStream = gulp.src(['dist/angular-smarter-models.min.js']);
+  return mergeStream(dependencyStream(), sourceStream, testFileStream())
+    .pipe(plugins.order(['**/*angular.js', 'dist/**/*.js']))
+    .pipe(plugins.jasmineBrowser.specRunner({ console: true }))
+    .pipe(plugins.jasmineBrowser.headless());
+});
+
+gulp.task('jasmine', function() {
+  return mergeStream(dependencyStream(), sourceFileStream(), testFileStream())
+    .pipe(plugins.order(['**/*angular.js', 'src/**/*.js']))
+    .pipe(plugins.jasmineBrowser.specRunner())
+    .pipe(plugins.jasmineBrowser.server({port: 8888}));
+});
+
+gulp.task('default', ['test']);
---
+++ test/models.spec.js
@@ -0,0 +1,5 @@
+describe( 'models', function() {
+  it( 'should work', function() {
+    expect( true ).toEqual( true );
+  } );
+} );
